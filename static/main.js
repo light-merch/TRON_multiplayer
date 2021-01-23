8d431ac0d7b67c16a671cf1f041eaddceff1d374
pavTiger <pavTiger@gmail.com>
2021-01-23 19:33:20 +0300

    Working camera & textures

--- static/main.js
+++ static/main.js
@@ -2,7 +2,8 @@ import * as THREE from "./three.module.js";
 
 import { OrbitControls } from "./OrbitControls.js";
 import { OBJLoader } from './OBJLoader.js';
-import { MTLLoader } from './jsm/loaders/MTLLoader.js';
+import { MTLLoader } from './MTLLoader.js';
+import { DDSLoader } from './DDSLoader.js';
 
 
 function httpGet(Url) {
@@ -63,25 +64,38 @@ function init() {
         camera.updateProjectionMatrix();
     });
 
+    const onProgress = function ( xhr ) {
 
-    const loader = new OBJLoader();
-    loader.load(
-        // resource URL
-        'models/lightcycle.obj',
-        // called when resource is loaded
-        function ( object ) {
-            window.bike = object;
-            scene.add( object );
-        },
-        // called when loading is in progresses
-        function ( xhr ) {
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-        },
-        // called when loading has errors
-        function ( error ) {
-            console.log( 'An error happened' );
+        if ( xhr.lengthComputable ) {
+
+            const percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+
         }
-    );
+
+    };
+    const onError = function () { };
+
+    const manager = new THREE.LoadingManager();
+    manager.addHandler( /\.dds$/i, new DDSLoader() );
+
+    new MTLLoader( manager )
+        .setPath( 'models/' )
+        .load( 'lightcycle.mtl', function ( materials ) {
+
+            materials.preload();
+
+            new OBJLoader( manager )
+                .setMaterials( materials )
+                .setPath( 'models/' )
+                .load( 'lightcycle.obj', function ( object ) {
+
+                    window.bike = object;
+                    scene.add( object );
+
+                }, onProgress, onError );
+
+        } );
 
     // light
     var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
@@ -95,7 +109,7 @@ function init() {
     var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.7);
     scene.add(ambientLight);
 
-    camera.position.set(-1.83222123889, 1.83199683912, -2.66435763809);
+    camera.position.set(0, 5, -6);
 
     camera.rotation.y = 3.14;
     camera.rotation.x = 0.6;
@@ -108,18 +122,7 @@ function init() {
 
 window.onload = function() {
     const FizzyText = function () {
-        this.num_particles = 0;
-        this.color = [255, 255, 0]; // RGB array
-        this.pointWidth = 1;
-
-        this.time = 0.0;
-        this.mode = "offline";
-        this.speedMode = "1.00";
         this.name = "Pavel Artushkov";
-
-        this.drawCube = true;
-        this.play = true;
-        this.turnOn = false;
 
         this.reset_defaults = function () {
             for (var i = 0; i < gui.__controllers.length; i++) {
@@ -128,8 +131,6 @@ window.onload = function() {
         };
     };
 
-    var FrameId = 1, direction = 1, arrayOfPoints = [], data;
-    window.default_size = 0.007;
 
     var stats = initStats(Stats);
 
@@ -142,29 +143,28 @@ window.onload = function() {
     });
 
 
-    // class Player {
-    //     constructor(x, y, z, rotation) {
-    //         this.x;
-    //         this.y;
-    //         this.z;
-    //         this.rotation = 0
-    //     }
-    // }
-    // var player = new Player(0, 0, 0, 0);
+    class Player {
+        constructor(last_trail_x, last_trail_y, last_trail_z) {
+            this.last_trail_x = last_trail_x;
+            this.last_trail_y = last_trail_y;
+            this.last_trail_z = last_trail_z;
+        }
+    }
+    var player = new Player(0, 1, -3);
 
 
     document.addEventListener("keydown", onDocumentKeyDown, false);
     function onDocumentKeyDown(event) {
         var keyCode = event.which;
         console.log(keyCode);
-        if (keyCode == 32) { // Space
-            fizzyText.play = !fizzyText.play;
-        } else if (keyCode == 65) { // A
+        if (keyCode === 32) { // Space
+            // fizzyText.play = !fizzyText.play;
+        } else if (keyCode === 65) { // A
             window.bike.rotation.z -= 0.1;
-        } else if (keyCode == 68) { // D
+        } else if (keyCode === 68) { // D
             window.bike.rotation.z += 0.1;
         }
-    };
+    }
 
 
     var tmp = init();
@@ -173,14 +173,24 @@ window.onload = function() {
     var camera = tmp[2];
     var controls = tmp[3];
 
-    // run game loop (update, render, repeat)
+    const trail_geometry = new THREE.BoxGeometry( 0.05, 1, 1 );
+    const trail_material = new THREE.MeshBasicMaterial( {color: 0x0fbef2} );
+
     var GameLoop = function() {
         requestAnimationFrame(GameLoop);
         stats.begin();
         controls.update();
 
-        // window.bike.position.z += 0.1;
-        // camera.position.z += 0.1;
+        if (window.bike !== undefined) {
+            window.bike.position.z += 0.1;
+            camera.position.z += 0.1;
+
+            controls.target.set(window.bike.position.x, window.bike.position.y, window.bike.position.z);
+
+            var trail = new THREE.Mesh(trail_geometry, trail_material);
+            trail.position.set(player.last_trail_x, player.last_trail_y, player.last_trail_z);
+            scene.add(trail);
+        }
 
         renderer.render(scene, camera); 
         stats.end();
